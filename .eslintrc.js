807adc36ae341438e61370734ac04e51b82f913a
francine <francinebc.mail@gmail.com>
2020-10-17 17:16:09 +1300

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,48 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2020': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react-hooks/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 11,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab',
+			{'SwitchCase': 1}
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		],
+		'@typescript-eslint/no-explicit-any': [
+			'off'
+		],
+		'@typescript-eslint/no-non-null-assertion': 'off',
+	}
+};
---
+++ src/app/client/components/Entrance.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { Fragment } from 'react'
+import './Entrance.css'
+
+const Entrance = () => {
+  return (
+      <Fragment>
+        <p className='hello'>hello</p>
+      </Fragment>
+  )
+}
+
+export default Entrance
